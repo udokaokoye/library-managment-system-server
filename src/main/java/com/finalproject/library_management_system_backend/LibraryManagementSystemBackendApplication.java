78850d2566d83d6e6c4e6c0738b5f359ba65bfee
Udoka Okoye <leviokoye@gmail.com>
2025-10-07 16:19:23 -0400

    init

---
+++ src/main/java/com/finalproject/library_management_system_backend/LibraryManagementSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.finalproject.library_management_system_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagementSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryManagementSystemBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finalproject/library_management_system_backend/LibraryManagementSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalproject.library_management_system_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
