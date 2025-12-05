edddc26c9a1d54b09e30f2631c4ff26b1a8d4e45
Levi Okoye <leviokoye@gmail.com>
2025-12-05 10:41:00 -0500

    Add JavaDoc comments to HomeController
    Added JavaDoc comments to HomeController class and home method.

--- src/main/java/com/finalproject/library_management_system_backend/controller/HomeController.java
+++ src/main/java/com/finalproject/library_management_system_backend/controller/HomeController.java
@@ -2,9 +2,18 @@ package com.finalproject.library_management_system_backend.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+/**
+ * Provides a basic endpoint for testing or confirming
+ * that the application is running.
+ */
 @RestController
 public class HomeController {
+
+        /**
+     * Returns a simple greeting message.
+     *
+     * @return a plain text "Hello World" string
+     */
     @GetMapping("/home")
     public  String home() {
         return "Hello World";
